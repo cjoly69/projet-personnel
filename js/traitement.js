55df445a18049641e9292c986b61dac940e75229
cjoly69 <cjoly.webdesign@gmail.com>
2016-11-07 15:01:12 +0100

    essai fonction ajouteChapitre

--- js/traitement.js
+++ js/traitement.js
@@ -8,3 +8,14 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 }
+
+//chapitres
+
+function ajouteChap(){
+  var chap = document.getElementsByClassName('chapitre');
+  var button = document.createElement('button');
+  var titre = document.getElementById('titre');
+  button.innerText = titre.value;
+  document.body.appendChild(button);
+  titre.value = '';
+}
