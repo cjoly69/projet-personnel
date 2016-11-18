0bf6bb6ba492745cbc4d954718f47d719ab64b1c
cjoly69 <cjoly.webdesign@gmail.com>
2016-11-18 12:52:34 +0100

    correction de l'appel de la fonction accordion() TODO les compétences a distribuer selon le chapitre

--- js/traitement.js
+++ js/traitement.js
@@ -1,85 +1,69 @@
 // accordion
-function accordion(){
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+function accordion() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-  for (i = 0; i < acc.length; i++) {
-      acc[i].onclick = function(){
-          this.classList.toggle("active");
-          this.nextElementSibling.classList.toggle("show");
-      }
-  }
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
 }
 
+//INITIALISATION ADAPTER L URL v
+getChapters("basededonnees.json");
+// getChapters("niveau3.csv");
 
-//chapitres
 
-function ajouteChap(){
-  var chap = document.getElementsByClassName('chapitre');
-  var button = document.createElement('button');
-  var titre = document.getElementById('titre');
-  button.innerText = titre.value;
-  document.querySelector(".new").appendChild(button);
-  accordion();
-  titre.value = '';
+//RECUPERATION DE DONNEES SUR UN FICHIER JSON AVEC JSON
+function getChapters(url) {
+
+    var xmlhttp = new XMLHttpRequest();
+    //var url = "./basededonnees.txt";
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            generateChapter(myArr);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
 }
 
-//compétences
-// function ajouteComp(){
-//   var comp = document.getElementsByClassName('competence');
-//   var button = document.createElement('input');
-//   var titreComp = document.getElementById('competence');
-//   input.innerText = titreComp.value;
-//   document.querySelector(".new").appendChild(input);
-//   titreComp.value = '';
-// }
+//FORMATAGE DES CHAPITRES ET AFFICHAGE
+function generateChapter(arrayJSONObj) {
+    //console.log(arrayJSONObj[0]["niveaux"]);
+    var out = "";
+    var i = 0;
+    var chapterArray = {};
 
-  // ajouteChap();
-  // ajouteComp();
+    //LE CHOIX DU NIVEAU EST ARBITRAIRE POUR L'INSTANT
+    for (niveau in arrayJSONObj[0]["niveaux"]) {
+        if (niveau == "3e") {
+            chapterArray = arrayJSONObj[0]["niveaux"][niveau]["chapitres"];
+            //console.log(chapterArray);
+        }
+        //else {console.log(niveau);}
+    }
+    for (chapter in chapterArray) {
+          accordion();
+        document.getElementById("liste_chapitres").innerHTML
+         += '<button class="accordion" onclick="accordion()">' + chapter
+         + '<i class="material-icons">keyboard_arrow_down</i></button>'
+         + '<div class="panel" id="pan">';
 
-  //INITIALISATION ADAPTER L URL v
-  getChapters("basededonnees.json");
-  // getChapters("niveau3.csv");
+        for (competence in chapterArray[chapter]["competences"]) {
+            i++;
+            //VOIR PROBLEME AVEC L'ATTRIBUT CHECKED TOUJOURS TRUE : checked=' + competence["etat"]
+            document.getElementById("pan").innerHTML += '<input type="checkbox" id="cbox' + i + '" value="deuxieme_checkbox" ' + '/> <label for="cbox' + i + ' "> ' + competence + '</label></br>';
+        }
+        document.getElementById("pan").innerHTML += '</div>';
 
-
-  //RECUPERATION DE DONNEES SUR UN FICHIER JSON AVEC JSON
-  function getChapters(url){
-
-  	var xmlhttp = new XMLHttpRequest();
-  	//var url = "./basededonnees.txt";
-
-  	xmlhttp.onreadystatechange = function() {
-  	    if (this.readyState == 4 && this.status == 200) {
-  	        var myArr = JSON.parse(this.responseText);
-  	        generateChapter(myArr);
-  	    }
-  	};
-  	xmlhttp.open("GET", url, true);
-  	xmlhttp.send();
-  }
-
-  //FORMATAGE DES CHAPITRES ET AFFICHAGE
-  function generateChapter(arrayJSONObj){
-  	//console.log(arrayJSONObj[0]["niveaux"]);
-      var out = "";
-      var i = 0;
-      var chapterArray = {};
-
-      //LE CHOIX DU NIVEAU EST ARBITRAIRE POUR L'INSTANT
-      for (niveau in arrayJSONObj[0]["niveaux"]){
-      	if (niveau == "3e"){
-     			chapterArray = arrayJSONObj[0]["niveaux"][niveau]["chapitres"];
-     			//console.log(chapterArray);
-      	}
-      	//else {console.log(niveau);}
-      }
-      for (chapter in chapterArray){
-      	document.getElementById("liste_chapitres").innerHTML += '<button class="accordion">' + chapter + '</button>' + '<div class="panel">' ;
-      	for(competence in chapterArray[chapter]["competences"]){
-      		i++;
-      		//VOIR PROBLEME AVEC L'ATTRIBUT CHECKED TOUJOURS TRUE : checked=' + competence["etat"]
-      		document.getElementById("liste_chapitres").innerHTML += '<input type="checkbox" id="cbox' + i + '" value="deuxieme_checkbox" '  + '/> <label for="cbox' + i +' "> '+ competence  +'</label></br>';
-  	   	}
-  	   	document.getElementById("liste_chapitres").innerHTML += '</div>'
-      }accordion();
-  }
+    }
+    //j'appelle la fonction pour l'appel au click
+    return accordion();
+}
