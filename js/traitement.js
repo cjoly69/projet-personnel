3a67ba676580bc632e18540a2e02bdc69b15ddfd
cjoly69 <cjoly.webdesign@gmail.com>
2016-11-19 11:25:06 +0100

    incrémentation des compétences + réglage création des chapitres et une compétence + localstorage + TODOrécup ajax sur le fichier csv

--- js/traitement.js
+++ js/traitement.js
@@ -39,6 +39,7 @@ function generateChapter(arrayJSONObj) {
     //console.log(arrayJSONObj[0]["niveaux"]);
     var out = "";
     var i = 0;
+    var comp = 0;
     var chapterArray = {};
 
     //LE CHOIX DU NIVEAU EST ARBITRAIRE POUR L'INSTANT
@@ -50,20 +51,54 @@ function generateChapter(arrayJSONObj) {
         //else {console.log(niveau);}
     }
     for (chapter in chapterArray) {
-          accordion();
+        comp++;
+        var idpan="pan"+comp;
+        accordion();
         document.getElementById("liste_chapitres").innerHTML
          += '<button class="accordion" onclick="accordion()">' + chapter
          + '<i class="material-icons">keyboard_arrow_down</i></button>'
-         + '<div class="panel" id="pan">';
+         + '<div class="panel" id="'+idpan+'">';
 
         for (competence in chapterArray[chapter]["competences"]) {
+            //console.log(chapterArray[chapter]);
             i++;
             //VOIR PROBLEME AVEC L'ATTRIBUT CHECKED TOUJOURS TRUE : checked=' + competence["etat"]
-            document.getElementById("pan").innerHTML += '<input type="checkbox" id="cbox' + i + '" value="deuxieme_checkbox" ' + '/> <label for="cbox' + i + ' "> ' + competence + '</label></br>';
+            document.getElementById(idpan).innerHTML += '<input type="checkbox" id="cbox' + i + '" value="deuxieme_checkbox" ' + '/> <label for="cbox' + i + ' "> ' + competence + '</label></br>';
         }
-        document.getElementById("pan").innerHTML += '</div>';
+        document.getElementById(idpan).innerHTML += '</div>';
 
     }
+    //ajout des chapitres créé sur le local storage
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.savedChapters) {
+            document.getElementById("liste_chapitres").innerHTML += localStorage.savedChapters;
+        }
+    }
+    
     //j'appelle la fonction pour l'appel au click
     return accordion();
+}
+
+//CREATION DE CHAPITRES
+function createChapter(){
+    var titre = document.getElementById("titre").value;
+    var competence = document.getElementById("competence").value;
+    var newchapter = '<button class="accordion">'+ titre +'<i class="material-icons">keyboard_arrow_down</i></button>'
+                    +'<div class="panel">'
+                        +'<input type="checkbox" value="premiere_checkbox"><label>'+ competence +'</label><br>'
+                    +'</div>';
+    document.getElementById("liste_chapitres").innerHTML += newchapter;
+    saveChapter(newchapter);
+    return accordion();
+}
+
+//SAUVEGARDE DU CHAPITRE EN LOCAL STORAGE
+function saveChapter(chapter) {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.savedChapters) {
+            localStorage.savedChapters += chapter;
+        } else {
+            localStorage.savedChapters = chapter;
+        }
+    }
 }
